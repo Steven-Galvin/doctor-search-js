86cccf5db9c31a50bb40972157138457fe79696e
Steven Galvin <steven.john.galvin@gmail.com>
2017-05-26 12:11:07 -0700

    succcessfully pull data from api. console log first and last name of single doctor as well as array of all doctors pulled from search result.

--- js/doctor-search-interface.js
+++ js/doctor-search-interface.js
@@ -1,5 +1,11 @@
 var Doctor = require('./../js/doctor-search.js').doctorModule;
 
 $(document).ready(function() {
-
+  var doctors = new Doctor();
+  $('#find-doctors').click(function(e) {
+    e.preventDefault();
+    var symptom = $('#symptom').val();
+    var test = doctors.getDoctors(symptom);
+    console.log(test);
+  });
 });
--- js/doctor-search.js
+++ js/doctor-search.js
@@ -1,4 +1,25 @@
 var apiKey = require('./../.env').apiKey;
 
+function Doctor(){
+}
+
+Doctor.prototype.getDoctors = function(symptom) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ symptom +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+    .then(function(result) {
+      console.log(result);
+      var doctors = [];
+      var test = result.data
+      test.forEach(function(doctor) {
+        doctors.push(doctor.profile.first_name);
+      });
+      console.log(doctors);
+      console.log(result.data[0].profile.first_name);
+      console.log(result.data[0].profile.last_name);
+    })
+   .fail(function(error){
+      console.log("fail");
+    });
+  // $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=' + lat + '%2C' + lon + '%2C%205&user_location=' + lat + '%2C' + lon + '&skip=0&limit=20&user_key=' + apiKey)
+};
 
 exports.doctorModule = Doctor;
